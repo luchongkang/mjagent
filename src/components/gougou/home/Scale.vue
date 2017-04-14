fde828e44d806e32fb30340cbec0ed6cf8644845
jasonlu <406118618@qq.com>
2017-04-14 17:56:15 +0800

    add something

--- src/components/gougou/home/Scale.vue
+++ src/components/gougou/home/Scale.vue
@@ -2,18 +2,39 @@
   <div class="scale">
     <divider>我的提成</divider>
     <div class="box">
-      <p>本月收益: {{number}}</p>
-      
+      <p style="margin-left:1em;">本月收益: {{number}}</p>
+      <table class="tab">
+          <thead>
+              <tr>
+                  <th>name</th>
+                  <th>type</th>
+                  <th>default</th>
+              </tr>
+          </thead>
+          <tbody>
+              <tr>
+              <th>1</th>
+              <th>2</th>
+              <th>3</th>
+              </tr>
+          </tbody>
+      </table>
+
     </div>
   </div>
 </template>
 
 <script>
+import { Divider } from 'vux'
 export default {
-  name: 'home',
+  components: {
+    Divider
+  },
+  name: 'scales',
   data () {
     return {
-      number: 0
+      number: 0,
+      show: true
     }
   },
   computed: {
@@ -26,7 +47,22 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.home {
-
+table {
+    display: block;
+    width: 100%;
+    overflow: auto;
+    margin: 1em 3em;
+    margin-bottom: 16px;
+        border-spacing: 0;
+    border-collapse: collapse;
+}
+table tr {
+    background-color: #fff;
+    border-top: 1px solid #ccc;
+}
+table th {
+    padding: 6px 13px;
+    border: 1px solid #ddd;
+    font-weight: 700;
 }
 </style>
