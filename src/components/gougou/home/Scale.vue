1804fcf75c13eded33c4b3af3d1ec1cfc46a4873
jasonlu <406118618@qq.com>
2017-08-01 18:53:31 +0800

    udpate something

--- src/components/gougou/home/Scale.vue
+++ src/components/gougou/home/Scale.vue
@@ -3,12 +3,12 @@
     <div class="box">
       <p>本月收益: {{number}}</p>
       <group title="按时间查询">
-        <datetime v-model="date" @on-change="change" cancelText="取消" confirmText="确定" title="日期："></datetime>
+        <datetime v-model="start" @on-change="change" cancelText="取消" confirmText="确定" title="开始时间："></datetime>
+        <datetime v-model="end" @on-change="changeEnd" cancelText="取消" confirmText="确定" title="结束时间："></datetime>
       </group>
       <div style="padding:15px;">
       <x-button type="primary" @click.native="search">查 询</x-button>
     </div>
-
       <table class="tab">
           <thead>
               <tr>
@@ -27,7 +27,7 @@
               </tr>
           </tbody>
       </table>
-      </table>
+      <p class="more" v-bind:class="{hide : isHide}" @click="getMore">查看更多</p>
     </div>
   </div>
 </template>
@@ -36,7 +36,10 @@
 import { Datetime, Group, XButton } from 'vux'
 export default {
   created () {
-    this.date = (new Date().toLocaleString()).substring(0, 9)
+    let date = new Date()
+    this.start = (date.toLocaleString()).substring(0, 9).replace(/\//g, '-')
+    this.end = this.start
+    // this.end = (new Date(date.getTime() + (1 * 24 * 60 * 60 * 1000)).toLocaleString()).substring(0, 9).replace(/\//g, '-')
   },
   components: {
     Datetime, Group, XButton
@@ -44,8 +47,12 @@ export default {
   name: 'scales',
   data () {
     return {
+      isHide: false,
       number: 0,
-      date: '',
+      start: '',
+      end: '',
+      page: 1,
+      pageCount: 0,
       list: []
     }
   },
@@ -60,16 +67,48 @@ export default {
       this.http_get('/home/get-rebate').then(res => {
         this.list = res.data.list
         this.number = res.data.number
+        this.pageCount = res.data.pageCount
+        console.log(this.pageCount)
+        if (this.pageCount === 0 || this.pageCount === 1) {
+          this.isHide = true
+        }
       })
     },
     change: function (value) {
-      this.date = value
+      this.start = value
+    },
+    changeEnd: function (value) {
+      this.end = value
     },
     search () {
-      this.http_get('/home/get-rebate?date=' + this.date).then(res => {
+      this.http_get('/home/get-rebate?start=' + this.start + '&end=' + this.end + ' 23:59:59').then(res => {
         this.list = res.data.list
         this.number = res.data.number
+        this.pageCount = res.data.pageCount
+        this.page = 1
+        if (this.pageCount === 1) {
+          this.isHide = true
+        } else {
+          this.isHide = false
+        }
       })
+    },
+    getMore () {
+      ++this.page
+      if (this.page <= this.pageCount) {
+        this.http_get('/home/get-rebate?start=' + this.start + '&end=' + this.end + ' 23:59:59' + '&page=' + this.page).then(res => {
+          this.list.push.apply(this.list, res.data.list)
+          this.number = res.data.number
+          this.pageCount = res.data.pageCount
+          if (this.pageCount === this.page) {
+            this.isHide = true
+          } else {
+            this.isHide = false
+          }
+        })
+      } else {
+        this.$vux.toast.show({text: '没有更多了'})
+      }
     }
   }
 }
@@ -77,6 +116,13 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.hide {
+  display: none;
+}
+.more {
+  color: #586C94;
+  font-size: 14px;
+}
 p {
   margin:1em;
 }
